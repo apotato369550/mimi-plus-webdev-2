b33a26a51773045305fb9aeed6e4f93450788a0d
DemonAcolyte <miggycarumba912@gmail.com>
2025-07-05 09:42:44 +0800

    feat: added verifications in rewards page

--- server/controllers/rewardsController.js
+++ server/controllers/rewardsController.js
@@ -7,19 +7,38 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_DATABASE
 
 
 });
 
+const queryAsync = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, params, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+};
 
-exports.viewRewards= async(req, res) => {
+
+exports.viewRewards = async (req, res) => {
   
- try {
-    const data = await db.query("SELECT * FROM rewards");
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'failed to fetch data'});
+  try {
+    const query = 'SELECT * FROM rewards';
+
+    const rewards = await queryAsync(query);
+
+    res.status(200).json({
+
+      message: "Rewards retrieved successfully",
+      data: rewards
+    });
+
+  }catch (error) {
+
+
+    res.status(500).json({ message: "Error retrieving rewards", error: error.message})
+
   }
 };
--- server/routes/rewards.js
+++ server/routes/rewards.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const rewardsController = require('../controllers/rewardsController');
-
+const verifyToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/', rewardsController.viewRewards);
+router.get('/', verifyToken.authenticateToken, rewardsController.viewRewards);
 
 module.exports = router;
 
