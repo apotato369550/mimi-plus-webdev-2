7fef458ba0287e8d5add2d125a7376805e7b10f9
DemonAcolyte <miggycarumba912@gmail.com>
2025-07-07 12:04:57 +0800

    chore: removed db in server.js and added to separate folder

--- server/server.js
+++ server/server.js
@@ -12,20 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Database Connection
 
-const db = mysql.createConnection({
-
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-
-
-});
-
-
-
+app.use('/transactions', require('./routes/transactions'));
 app.use('/rewards', require('./routes/rewards'));
 app.use('/', require('./routes/authentication'));
 const PORT = process.env.DB_PORT;
