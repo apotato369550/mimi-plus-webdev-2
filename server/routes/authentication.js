5f0a1d46751f80982431c549ff1af8676d0b0235
DemonAcolyte <miggycarumba912@gmail.com>
2025-07-09 10:28:16 +0800

    patch: added email verification and forget password

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 router.post('/', authController.login)
 router.post('/register', authController.register)
+router.get('/verify-email/:token', authController.verifyEmail);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password/:token', authController.resetPassword);
 module.exports = router;
 
 
