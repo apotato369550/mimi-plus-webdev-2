45fb5f38c717b643d3f45c4ec257fb8c161015c8
DemonAcolyte <miggycarumba912@gmail.com>
2025-07-18 08:25:00 +0800

    chore: separated admin and staff routes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = express();
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const staffRoutes = require("./routes/staff");
 
 /*******************************************************************
  *                     All predefined routes                                 *
@@ -14,11 +13,19 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/admin", require("./routes/admin"));
+//Admin Routes
+app.use("/api/admin", require("./admin-routes/home"));
+app.use("/api/admin/customers", require("./admin-routes/customers"));
+app.use("/api/admin/rewards", require("./admin-routes/rewards"));
+
+//Staff Routes
+app.use("/api/staff", require("./staff-routes/staff"));
+
+
+//Customer Routes
 app.use("/api/home", require("./routes/home"));
 app.use("/api/transactions", require("./routes/transactions"));
 app.use("/api/rewards", require("./routes/rewards"));
-app.use("/api/staff", staffRoutes);
 app.use("/api/auth", require("./routes/authentication"));
 
 const PORT = process.env.PORT || 5002;
